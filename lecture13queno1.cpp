7b79be4a2165149c0a462d69b7cbbcd4feb4bcb7
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-09-11 18:03:15 +0530

    Update lecture13queno1.cpp

--- lecture13queno1.cpp
+++ lecture13queno1.cpp
@@ -1,58 +1,52 @@
 galat queston not comes output 
 
 #include<vector>
-using namespace std;
-int firstOcc(vector<int>& arr, int n, int key) {
-
-    int s = 0, e = n-1;
-    int mid = s + (e-s)/2;
+ using namespace std;
+ int firstOcc( vector<int> & arr , int n, int key){
+    int s=0, e =n-1;
+    int mid = s +(e-s)/2;
     int ans = -1;
-    while(s<=e) {
-
-        if(arr[mid] == key){
-            ans = mid;
-            e = mid - 1;
+    while ( s<=e){
+        int (arr[mid]==key){
+        ans =mid;
+        e = mid-1;
         }
-        else if(key > arr[mid]) {//Right me jao
-            s = mid + 1;
+        else if (key>arr[mid]){
+            s=mid+1;
         }
-        else if(key < arr[mid]) {//left me jao
-            e = mid - 1;
+        else if (key < arr[mid]){
+            e=mid -1;
         }
-
-        mid = s + (e-s)/2;
+        mid= s+(e-s)/2;
     }
     return ans;
-}
-
-int lastOcc(vector<int>& arr, int n, int key) {
-
-    int s = 0, e = n-1;
-    int mid = s + (e-s)/2;
-    int ans = -1;
-    while(s<=e) {
-
-        if(arr[mid] == key){
+ }
+  int lastOcc( vector <int> & arr, int n , int key){
+    int s=0, e = n-1;
+    int mid = s+(e-s)/2;
+    int ans =-1;
+    while(s<=e){
+        if (arr[mid]== key){
             ans = mid;
-            s = mid + 1;
+            s = mid +1;
         }
-        else if(key > arr[mid]) {//Right me jao
-            s = mid + 1;
+        else if  (key > arr[mid]){
+            s=mid+1;
         }
-        else if(key < arr[mid]) {//left me jao
-            e = mid - 1;
+        else if (key>arr[mid]){
+            e=mid-1;
         }
-
-        mid = s + (e-s)/2;
+        mid=s+(e-s)/2;
     }
-    return ans;
-}
+    mid= s+(e-s)/2;
+  }
+   return ans;
+ }
+  
+   pair <int , int > firstAndlastPosition (vector<int> & arr, int n, int key){
+    pair<int , int> p;
+    p.first= firstocc(arr,n,key);
+    p.second= lastOcc(arr, n, key);
 
-pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
-{
-    pair<int,int> p;
-    p.first = firstOcc(arr, n, k);
-    p.second = lastOcc(arr, n, k);
-    
     return p;
-}
+   }
